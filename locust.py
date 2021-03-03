73639cdfba18a789480c9749df76fb74ed440f31
Roopeshvarmanampally <roopeshvarma.nampally@eaglegenomics.com>
2021-03-03 23:46:32 +0530

    added

--- locust.py
+++ locust.py
@@ -11,3 +11,9 @@ class QuickstartUser(HttpUser):
     @task
     def hello_world(self):
         self.client.get("/Dashboard")--add
+
+
+class QuickstartUser(HttpUser):
+    @task
+    def hello_world(self):
+        self.client.get("/Dashboard")--add
