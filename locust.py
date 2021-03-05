19c9372b73f66ffacee7774f6a58d66154007160
Roopeshvarmanampally <67359665+Roopeshvarmanampally@users.noreply.github.com>
2021-03-05 16:06:36 +0530

    sds
    sdlkksl

--- locust.py
+++ locust.py
@@ -24,3 +24,9 @@ class QuickstartUser(HttpUser):
     @task
     def hello_world(self):
         self.client.get("/Dashboard")--add
+
+        
+        class QuickstartUser(HttpUser):
+    @task
+    def hello_world(self):
+        self.client.get("/Dashboard")--add
